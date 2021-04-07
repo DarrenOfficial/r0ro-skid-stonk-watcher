3f958e60c30687580b0e58abffc790d1d08735fd
Darren Official <darrennathanaelofficial@gmail.com>
2021-04-07 08:53:39 -0400

    initial commit

---
+++ src/bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`hello world`);
+});
+
+
+client.login('ODI5MzM4MDI2NjE3NjAyMDc4.YG2rOQ.m6829LiQetIFjAey9Pb8dYlKf7M');
